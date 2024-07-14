8d5e294c45084c0672ec6fd6b9186560bc73394c
Takumi Mori <51111242+takumi3488@users.noreply.github.com>
2024-07-14 22:25:45 +0900

    fix: Docker Desktopが終了するようにした

--- switch.zsh
+++ switch.zsh
@@ -1,10 +1,14 @@
 #!/bin/zsh -e
-QUIT_APP="Docker"
-ACTIVATE_APP="OrbStack"
+QUIT_APP="/Applications/Docker.app/Contents/MacOS/Docker Desktop.app/Contents/MacOS/Docker Desktop"
+ACTIVATE_APP="/Applications/OrbStack.app/Contents/MacOS/OrbStack"
+CONTEXT="orbstack"
 if [ $(docker context show) = "orbstack" ]; then
-  QUIT_APP="OrbStack"
-  ACTIVATE_APP="Docker"
+  QUIT_APP="/Applications/OrbStack.app/Contents/MacOS/OrbStack"
+  ACTIVATE_APP="/Applications/Docker.app/Contents/MacOS/Docker Desktop.app/Contents/MacOS/Docker Desktop"
+  CONTEXT="desktop-linux"
 fi
 
-osascript -e "tell application \"${QUIT_APP}\" to quit"
-osascript -e "tell application \"${ACTIVATE_APP}\" to activate"
+ps axww | grep "${QUIT_APP}" | grep -v grep | perl -lane 'print $F[0]' | xargs kill
+nohup "${ACTIVATE_APP}"  > /dev/null 2>&1 < /dev/null &
+
+docker context use ${CONTEXT}
